679712b6eb35e5a96c58486d8a8d923aea4afd47
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-05-09 15:31:31 -0600

    add Header.jsx  and  CenterMenu.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './components/Header'
 
 
 function App() {
   return (
-    <div className="App">
-      I Am Here
+    <div className="App text-white overflow-hidden">
+      <Header/>
     </div>
   );
 }
