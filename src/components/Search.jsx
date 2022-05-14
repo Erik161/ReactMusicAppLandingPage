7f18d3a599036233152ace450962cb856be0f958
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-05-14 10:35:51 -0600

    add Search.jxs modiffied right Side

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,7 +8,7 @@ function Search() {
             <img
             src={require("../img/backgraphics.png")}
             alt=''
-            className="absolute top-[22rem] left-[-47rem]"
+            className="absolute top-[15rem] left-[-47rem] z-[0]"
             />
             <img
             src={require("../img/d1.png")}
@@ -32,16 +32,16 @@ function Search() {
             />
         </div>
         {/* Right Side */}
-        <div className="right flex items-start flex-col justify-start flex-1 h-[100%] pt-[9rem]">
+        <div className="right flex items-start flex-col justify-start flex-1 h-[100%] pt-[9rem] z-[40]">
             {/* search */}
             <div className="searchbar flex justify-start w-[100%]">
               <input 
               type="text" 
               placeholder="Enter the keyword or URL" 
               className="flex-[19] outline-none bg-[#020917] rounded-xl p-3 h-[3rem]"
-              
               />
-              <div className="searchIcon flex flex-1 items-center rounded-xl ml-4 bg-gradient-to-bl from-[#F3071D] to-[#E600ff] p-4 h-[3rem]">
+              {/* SearchIcon */}
+              <div className="searchIcon flex flex-1 items-center rounded-xl ml-4 bg-gradient-to-bl from-[#F3071D] to-[#E600FF] p-4 h-[3rem]">
                 <img 
                 src={require ("../img/search.png")} 
                 alt="" 
@@ -52,13 +52,24 @@ function Search() {
             {/* tild icon */}
             <div className="tild flex justify-start mt-7 items-center w-[100%]">
               <img
-                src={require("../img/search.png")}
+                src={require("../img/Path 318.png")}
                 alt="" 
-                className="w-[1.5rem]"
-                
-    
+                className="w-[5rem]"
               />
             </div>
+            {/* Paragraph */}
+            <div className="detail flex flex-col mt-5 text-4xl">
+              <span>Search Music by</span>
+              <span>
+                <b>Name or Direct URL</b>
+              </span>
+              <span className="text-sm mt-3 text-[#4D586A]">
+                Duis feugiat congue metus, ultrices vulputate <br/> nibh viverra
+                eget. Vestibulum ullamcorper <br/> volutpat varius.
+              </span>
+            </div>
+            {/* Music Player */}
+            <MusicPlayer/>
         </div>
     </div>
   );
