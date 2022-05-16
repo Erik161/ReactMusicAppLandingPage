2dbcbc479464916f075f088741dd8e4df571050e
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-05-15 23:39:21 -0600

    add file Download.jsx

---
+++ src/components/Download.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Download() {
+  return (
+    <div className="flex flex-col items-center justify-start px-[5rem] bg-[#020917] h-[48rem] pt-[18rem] mt-[-10rem] relative z-[0] rounded-b-[5rem]">
+        {/* tild icon or path icon */}
+        <img src={require("../img/Path 318.png")} alt="" className="w-[5rem]"/>
+        {/* heading */}
+        <div className="headline mt-7 flex flex-col items-center text-[2rem]">
+            <span> Download The Best Music </span>
+            <span>
+                <b>App Now!</b>
+            </span>
+            <span className="text-[1rem] text-gray-400 px-[15rem] text-center mt-[1rem]">
+            Duis feugiat congue metus, ultrices vulputate nibh viverra eget.
+            Vestibulum ullamcorper volutpat varius.
+            </span>
+        </div>
+
+    </div>
+  )
+}
+
+export default Download
