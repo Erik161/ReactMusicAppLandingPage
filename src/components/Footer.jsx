b13638302583aa024da7e3ce56f2f73388acdef1
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-05-17 06:50:12 -0700

    add file Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CenterMenu from './CenterMenu'
+
+function Footer() {
+  return (
+    <div className="footer">
+        <CenterMenu/>
+        {/* Social Icons */}
+        <div className="flex w-[100%] justify-center mt-14">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Footer
