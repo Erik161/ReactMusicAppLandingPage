99ddc7cf2f34ed31e3d9e69d08252f54243ca99a
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-05-19 00:39:49 -0600

    add UseState, react-visibility-sensor, framer-motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,48 @@
 import { React, useState } from "react";
 import DownloadAds from "./DownloadAds";
 import VisibilitySensor from "react-visibility-sensor";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 function Hero() {
   const [elementIsVisible, setElementIsVisible] = useState(false);
-  
+  const bg = {
+    true: {
+      left: "7rem",
+    },
+    false: {
+      left: "19rem",
+    },
+  };
+  const musicPlayer = {
+    true: {
+      left: "295px",
+    },
+    false: {
+      left: "235px",
+    },
+  };
+  const rect = {
+    true: {
+      left: "11rem",
+    },
+    false: {
+      left: "13rem",
+    },
+  }
+  const heart = {
+    true: {
+      left: "9rem",
+    },
+    false: {
+      left: "12.5rem",
+    },
+  };
   return (
-
-    <VisibilitySensor>
-      <div className="wrapper bg-[#081730] flex items-center justify-between px-[5rem] rounded-b-[5rem] w-[100%] h-[37.5rem] relative z-[3]">
+    <VisibilitySensor
+      onChange={(isVisible) => setElementIsVisible(isVisible)}
+      minTopValue={300}
+    >
+      <div className="wrapper bg-[#081730] flex items-center justify-between px-[5rem] rounded-b-[5rem] w-[100%] h-[35rem] relative z-[3]">
         {/* left side */}
         <div className="headings flex flex-col items-start justify-center h-[100%] text-[3rem]">
           <span>Experience The</span>{" "}
@@ -29,7 +62,10 @@ function Hero() {
         </div>
         {/* right side */}
         <div className="images relative w-[50%]">
-          <img
+          <motion.img
+            variants={bg}
+            animate={`${elementIsVisible}`}
+            transition={{ duration: 1, type: "ease-out" }}
             src={require("../img/backgraphics.png")}
             alt=""
             className="absolute top-[-8rem] left-[19rem]"
@@ -39,17 +75,35 @@ function Hero() {
             alt=""
             className="absolute top-[-15rem] h-[34rem] left-[13rem]"
           />
-          <img
+          <motion.img
+            variants={musicPlayer}
+            animate={`${elementIsVisible}`}
+            transition={{
+              duration: 1,
+              type: "ease-out",
+            }}
             src={require("../img/p 2.png")}
             alt=""
             className="absolute left-[235px] top-[94px] w-[175px]"
           />
-          <img
+          <motion.img
+            variants={rect}
+            animate={`${elementIsVisible}`}
+            transition={{
+              type: "ease-out",
+              duration: 1,
+            }}
             src={require("../img/p 3.png")}
             alt=""
             className="absolute w-[5rem] left-[13rem] top-[12rem]"
           />
-          <img
+          <motion.img
+            variants={heart}
+            animate={`${elementIsVisible}`}
+            transition={{
+              type: "ease-out",
+              duration: 1,
+            }}
             src={require("../img/p 4.png")}
             alt=""
             className="absolute w-[5rem] left-[12.5rem] top-[12rem]"
@@ -57,8 +111,6 @@ function Hero() {
         </div>
       </div>
     </VisibilitySensor>
-    
-    
   );
 }
 
